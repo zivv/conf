a03d8b6e7e5fe39df96ad2a042c9bae8011e562c
ziv <zivvv0@gmail.com>
2013-08-26 20:08:07 +0800

    modified set.sh

--- set.sh
+++ set.sh
@@ -4,3 +4,8 @@ cp .gitconfig ~/
 cp .gitignore_global ~/
 cp .tmux.conf ~/
 cp .vimrc ~/
+if [ -f ~/.at_google ]; then
+  echo "at google"
+  cp g.bash_local ~/.bash_local
+  cp g.vimrc_local ~/.vimrc_local
+fi
