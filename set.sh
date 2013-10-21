afda7bc16c19928f3370e69b33a6ae766a066e58
ziv <zivvv0@gmail.com>
2013-10-21 14:26:46 +0800

    -.-

--- set.sh
+++ set.sh
@@ -5,8 +5,14 @@ cp .gitignore_global ~/
 cp .tmux.conf ~/
 cp .vimrc ~/
 cp -r skeletons ~/.vim/
+
 if [ -f ~/.at_google ]; then
   echo "at google"
   cp g.bash_local ~/.bash_local
   cp g.vimrc_local ~/.vimrc_local
+fi
+
+if [ -f ~/.at_z_lenovo ]; then
+  echo "at z_lenovo"
+  cp z_lenovo.vimrc_path ~/.vimrc_path
 fi
