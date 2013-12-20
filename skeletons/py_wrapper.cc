cae229a16c4b6b0c1aae1b018227024fcab38fe1
ziv <zivvv0@gmail.com>
2013-12-20 18:47:39 +0800

    modified wrapper

--- skeletons/py_wrapper.cc
+++ skeletons/py_wrapper.cc
@@ -1,16 +1,15 @@
 #include <Python.h>
 
-PyObject* wrap_func(PyObject* self, PyObject* args) {
+PyObject* wrap_Func(PyObject* self, PyObject* args) {
   Py_INCREF(Py_None);
   return Py_None;
 }
 
 static PyMethodDef methods[] = {
-  {"func", wrap_func, METH_VARARGS, "describtion"},
+  {"func", wrap_Func, METH_VARARGS, "describtion"},
   {NULL, NULL}
 };
 
 PyMODINIT_FUNC initmodule() {
-  PyObject* m;
-  m = Py_InitModule("module", methods);
+  Py_InitModule("module", methods);
 }
