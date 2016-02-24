608a65f72f8e72609eb95fc39276cda2e25c5e2f
ziv <zivvv0@gmail.com>
2016-02-24 18:21:42 +0800

    update set.sh

--- set.sh
+++ set.sh
@@ -1,28 +1,59 @@
 #!/bin/bash
 
+# update files by last modification time
+
 files=(
 ".bash_base"
 ".tmux.conf"
 ".vimrc"
 ".gitconfig" ".gitignore_global"
 ".mongorc.js"
+"jupyter_notebook_config.py"
 )
+
+locas=(
+"z_lenovo"
+"z_mac"
+)
+
+z_lenovo=(
+".bash_local"
+".vimrc_path"
+)
+
+z_mac=(
+".bash_local"
+".vimrc_local"
+)
+
+diff_time() {
+  echo $(( $(stat -c %Y $1) - $(stat -c %Y $2) ))
+}
 
 for file in ${files[@]}
 do
-  cp $file ~/
+  if [[ $(diff_time ${file} ~/${file}) -gt 0 ]]; then
+    cp -v ${file} ~/
+  fi
 done
 
-cp -r vim/* ~/.vim/
+for file in $(find vim -type f)
+do
+  if [[ $(diff_time ${file} ~/.vim/${file#vim/}) -gt 0 ]]; then
+    cp -v ${file} ~/.vim/${file#vim/}
+  fi
+done
 
-if [ -f ~/.at_z_lenovo ]; then
-  echo "at z_lenovo"
-  cp z_lenovo.bash_local ~/.bash_local
-  cp z_lenovo.vimrc_path ~/.vimrc_path
-fi
-
-if [ -f ~/.at_z_mac ]; then
-  echo "at z_mac"
-  cp z_mac.bash_local ~/.bash_local
-  cp z_mac.vimrc_local ~/.vimrc_local
-fi
+for loca in ${locas[@]}
+do
+  if [ -f ~/.at_${loca} ]; then
+    eval "loca_files=\$\{${loca}\[\@\]\}"
+    eval "loca_files=$loca_files"
+    for file in ${loca_files[@]}
+    do
+      if [[ $(diff_time ${loca}${file} ~/${file}) -gt 0 ]]; then
+        cp -v ${loca}${file} ~/${file}
+      fi
+    done
+  fi
+done
