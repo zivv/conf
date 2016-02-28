a2b16c5a3f28587bb9f9b949b4695a129f1c0d2d
ziv <zivvv0@gmail.com>
2016-02-28 21:26:37 +0800

    update bash in OSX

--- set.sh
+++ set.sh
@@ -25,42 +25,28 @@ z_mac=(
 ".sh_local"
 )
 
-diff_time() {
-  if [[ ! -f $1 ]]; then
-    echo '0'
-  else
-    if [[ ! -f $2 ]]; then
-      echo '1'
-    else
-      echo $(( $(stat -c %Y $1) - $(stat -c %Y $2) ))
-    fi
-  fi
-}
-
 for file in ${files[@]}
 do
-  if [[ $(diff_time ${file} ~/${file}) -gt 0 ]]; then
-    cp -v ${file} ~/
-  fi
+    cp -uv ${file} ~/
 done
 
 for file in $(find vim -type f)
 do
-  if [[ $(diff_time ${file} ~/.vim/${file#vim/}) -gt 0 ]]; then
-    cp -v ${file} ~/.vim/${file#vim/}
+  new_file=${HOME}'/.vim/'${file#vim/}
+  if [[ ! -d ${new_file%/*} ]]; then
+    mkdir -p ${new_file%/*}
   fi
+  cp -uv ${file} ${new_file}
 done
 
 for loca in ${locas[@]}
 do
-  if [ -f ~/.at_${loca} ]; then
+  if [[ -f ~/.at_${loca} ]]; then
     eval "loca_files=\$\{${loca}\[\@\]\}"
     eval "loca_files=$loca_files"
     for file in ${loca_files[@]}
     do
-      if [[ $(diff_time ${loca}${file} ~/${file}) -gt 0 ]]; then
-        cp -v ${loca}${file} ~/${file}
-      fi
+      cp -uv ${loca}${file} ~/${file}
     done
   fi
 done
