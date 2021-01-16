bcfd08993e57f5e76ee9dca23385b076f3482d54
ziv-xj18 <zivvv0@gmail.com>
2021-01-16 17:00:14 +0800

    Update setup_ubuntu.sh

--- README.md
+++ README.md
@@ -10,11 +10,16 @@ Use the existence of file `~/.at_YOUR_PLACE`, checked by `set.sh`, to distinguis
 
 Set terminal to use **Solarized** theme.
 
-#### iTerm2 installation
+#### iTerm2
 
 1.  Download [solarized.zip](http://ethanschoonover.com/solarized/files/solarized.zip).
 2.  Unzip it, change to path `solarized/iterm2-colors-solarized/` and open `Solarized Dark.itermcolors`/`Solarized Light.itermcolors`.
 3.  Open iTerm2 and set `Preference`->`Profiles`->`Colors`->`Color Presets` as `Solarized Dark`.
+
+#### GNOME Terminal
+
+Open `Terminal`->`Preferences`->`Colors`, set `Built-in schemes` under `Text and Background Color` as
+`Solarized dark` and `Built-in schemes` under `Palette` as `Solarized`.
 
 ### Statusline & Prompt - [Powerline](https://github.com/powerline/powerline)
 
---
+++ tools/setup_ubuntu.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+#
+# To setup through SSH:
+#   scp tools/setup_ubuntu.sh $SERVER:/tmp && ssh -t $SERVER '/tmp/setup_ubuntu.sh'
+
+set -ex
+
+sudo apt install -y software-properties-common curl python3-pip tmux xclip tree git vim
+pip3 install psutil powerline-status trash-cli
+
+if (($(echo "$(
+  vim --version | grep "IMproved" | sed 's/.*IMproved \(.*\) (.*/\1/'
+) < 8.2" | bc -l))); then
+  # To install vim 8.2:
+  sudo add-apt-repository ppa:jonathonf/vim
+  sudo apt update
+  sudo apt install vim-gtk3=2:8.2\*
+fi
+
+# Optional:
+# sudo apt install -y build-essential cmake gcc g++ exuberant-ctags
+# sudo apt install -y net-tools ibus-pinyin mosh netcat-openbsd moreutils rename
+
+if [[ ! -f ~/.dircolors.256dark ]]; then
+  curl -Lo ~/.dircolors.256dark \
+    https://raw.github.com/seebi/dircolors-solarized/master/dircolors.256dark
+fi
+
+# https://github.com/junegunn/fzf
+# https://github.com/junegunn/fzf#using-linux-package-managers
+# sudo apt install fzf
+# NOTE: Key bindings (CTRL-T / CTRL-R / ALT-C) and fuzzy auto-completion may not be enabled by default.
+#       Refer to the package documentation for more information. (e.g. apt-cache show fzf)
+# https://github.com/junegunn/fzf#using-git
+if [[ ! -d ~/.fzf ]]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --key-bindings --completion --no-update-rc
+fi
+# https://github.com/sharkdp/fd
+if ! which fd >/dev/null; then
+  sudo apt install fd-find || (
+    # https://github.com/sharkdp/fd/releases
+    curl -Lo /tmp/fd.deb github.com/sharkdp/fd/releases/download/v8.1.1/fd_8.1.1_amd64.deb
+    sudo dpkg -i /tmp/fd.deb
+  )
+fi
+# https://github.com/BurntSushi/ripgrep
+if ! which rg >/dev/null; then
+  curl -Lo /tmp/rg.deb \
+    https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb
+  sudo dpkg -i /tmp/rg.deb
+fi
+# https://github.com/sharkdp/bat
+if ! which bat >/dev/null; then
+  curl -Lo /tmp/bat.deb \
+    https://github.com/sharkdp/bat/releases/download/v0.17.1/bat_0.17.1_amd64.deb
+  sudo dpkg -i /tmp/bat.deb
+fi
+# https://github.com/dandavison/delta
+if ! which delta >/dev/null; then
+  curl -Lo /tmp/delta.tar.gz \
+    https://github.com/dandavison/delta/releases/download/0.5.0/delta-0.5.0-x86_64-unknown-linux-gnu.tar.gz
+  tar xvf /tmp/delta.tar.gz -C /tmp
+  sudo mv /tmp/delta-0.5.0-x86_64-unknown-linux-gnu/delta /usr/local/bin
+fi
+
+if ! which zsh >/dev/null; then
+  sudo apt-get install zsh && touch ~/.zshrc && chsh -s $(which zsh)
+  curl -Lo ~/.zsh_antigen git.io/antigen
+  echo "[[ -f ~/.sh_base ]] && . ~/.sh_base" >>~/.zshrc && source ~/.zshrc
+fi
+
+if [[ ! -d ~/conf ]]; then
+  git clone --depth=1 https://github.com/zivv/conf ~/conf &&
+    (cd ~/conf && ./set.sh)
+fi
+
+# Recommend: rsync -avhP ~/.vim $SERVER:
